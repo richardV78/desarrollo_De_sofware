9be66fda5aea32cb6be14ef36f9af2a17aedfb38
richard <richardvalles94@gmail.com>
2024-07-16 17:09:27 -0600

    in progres

--- snake_game.py
+++ snake_game.py
@@ -1,9 +1,9 @@
 import pygame, sys, time, random
 # se importa una libreria de pyton para en su mayoria juegos
 pygame.init() 
-# se inicializa la libreria
-play_surface = pygame.display.set_mode((500, 500)) # aca se crea una variable que es el tamaño de la pantalla  
-fps = pygame.time.Clock() # se crea una variable que son los fps a las que se mueve 
+
+play_surface = pygame.display.set_mode((500, 500)) 
+fps = pygame.time.Clock()
 
 def food():
     random_pos = random.randint(0,49)*10
@@ -11,18 +11,14 @@ def food():
     return food_pos
 
 def main():
-# creamos el main y dos variables la cabeza y el cuerpo de la serpiente
-    snake_pos = [100,50]
-    snake_body = [[100,50], [90,50], [80,50]]
-    # estos array son para delimitar el tamaño de la ser4piente recien se inicie el juego 
-    # es en la posicion y tamaño que iniciara 
+
+    snake_pos = [100, 50]
+    snake_body = [[100,50],[90,50],[80,50]]
     change = "RIGHT"
-
     run = True
-
     food_pos = food()
-    score = 0 
-     
+    score = 0
+
     while run: 
 
         for event in pygame.event.get():
@@ -57,6 +53,7 @@ def main():
 
 
         play_surface.fill((0,0,0))
+        
         for pos in snake_body:
             pygame.draw.rect(play_surface,(200,200,200), pygame.Rect(pos[0], pos[1], 10, 10))
 
